f7d6dd960f0235fcaccfa9ce693fdf5c9dfb8ca9
pamungkas andono <pamungkas.org@gmail.com>
2021-03-30 11:21:24 +0700

    done

--- app/src/main/java/com/example/androidmytablayout/MainActivity.kt
+++ app/src/main/java/com/example/androidmytablayout/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.androidmytablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionPageAdapter = SectionPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionPageAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        supportActionBar?.elevation = 0f
     }
 }
