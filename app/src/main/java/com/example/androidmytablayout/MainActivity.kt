186a06514126cdef4885222e00e582d87d82d3db
pamungkas andono <pamungkas.org@gmail.com>
2021-03-30 11:54:46 +0700

    done lah

--- app/src/main/java/com/example/androidmytablayout/MainActivity.kt
+++ app/src/main/java/com/example/androidmytablayout/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.androidmytablayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
